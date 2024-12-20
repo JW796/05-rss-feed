c5bfce12b9f2e88f00694c7f5d274e782a2a969f
James Joseph <98562132+JW796@users.noreply.github.com>
2024-12-20 14:30:35 -0500

    WIP 2 Display data and frontend styling

--- src/App.js
+++ src/App.js
@@ -26,14 +26,22 @@ function App() {
       <h2 className="text-3xl font-semibold text-center mt-2 mb-4">
         Good morning, James
       </h2>
-      {articles.map((item, i) => (
-        <Feed
-          key-={i}
-          title={item.item.title}
-          link={item.item.link}
-          date={item.item.pubDate}
-        />
-      ))}
+      <div className="w-3/4 max max-w-lg border mx-auto p-5 rounded-xl">
+        <div className="bg-black flex flex-col items-center rounded-lg py-2 px-6 mb-5">
+          <img
+            src="https://cdn-images-1.medium.com/max/303/1*rOPLUJ3W6FUA3rO1U1IeuA@2x.png"
+            alt=""
+          />
+        </div>
+        {articles.map((item, i) => (
+          <Feed
+            key-={i}
+            title={item.item.title}
+            link={item.item.link}
+            date={item.item.pubDate}
+          />
+        ))}
+      </div>
     </>
   );
 }
--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1,9 +1,10 @@
 const Feed = ({ title, link, date }) => {
+
   let formatted = { day: "numeric", month: "long", year: "numeric" };
   let articleDate = new Date(date).toLocaleDateString("en-US", formatted);
   return (
     <>
-      <h3>{title}</h3>
+      <h3 className="text-xl mb-1">{title}</h3>
       <p>{articleDate}</p>
     </>
   );
